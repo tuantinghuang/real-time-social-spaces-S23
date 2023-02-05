a47d9ecea4fecadaed0e0b9ba81178b05f6fec97
tuantinghuang <th2703@nyu.edu>
2023-02-05 16:36:49 -0500

    updates

--- 2-material/index.js
+++ 2-material/index.js
@@ -1,36 +1,27 @@
 import * as THREE from 'three'; // * as in everything
-import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 import { RGBELoader } from 'https://threejs.org/examples/jsm/loaders/RGBELoader.js';
-
 import openSimplexNoise from 'https://cdn.skypack.dev/open-simplex-noise';
 import { Sky } from 'https://threejs.org/examples/jsm/objects/Sky.js';
 
-let canvas = document.querySelector('canvas.webgl')
+//set up scene
+let canvas = document.querySelector('canvas.webgl');
+
 let renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     alpha: true
 });
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 let scene = new THREE.Scene();
-//let camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 let cam_r = 3;
-//let cam_r = 3;
-//camera.position.z = cam_r;
-//camera.lookAt(0, 0, 0);
-
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000000);
 camera.position.set(0, 0, cam_r);
 camera.lookAt(0, 0, 0);
 
-//make ball
 
-//load hdr
-let hdrEquirect = new RGBELoader().load("assets/syferfontein_1d_clear_puresky_4k.hdr",
-    () => {
-        hdrEquirect.mapping = THREE.EquirectangularReflectionMapping
-    })
+//make ball
 
 
 let geo = new THREE.SphereGeometry(1.5, 64, 64);
@@ -40,6 +31,12 @@ for (let i = 0; i < geo.attributes.position.count; i++) {
     v3.fromBufferAttribute(geo.attributes.position, i);
     geo.positionData.push(v3.clone());
 }
+
+//load hdr
+let hdrEquirect = new RGBELoader().load("assets/syferfontein_1d_clear_puresky_4k.hdr",
+    () => {
+        hdrEquirect.mapping = THREE.EquirectangularReflectionMapping
+    });
 
 let mat = new THREE.MeshPhysicalMaterial({
     roughness: 0,
@@ -53,21 +50,15 @@ let ball = new THREE.Mesh(geo, mat);
 scene.add(ball);
 
 
-let plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshPhysicalMaterial({
-    color: 0xaaaaaa,
-    side: THREE.DoubleSide
-
-}));
-
 // Add Sky
+// code snippets from https://threejs.org/examples/?q=sky#webgl_shaders_sky
 let sky = new Sky();
 sky.scale.setScalar(450000);
 scene.add(sky);
 
 let sun = new THREE.Vector3();
 
-/// GUI
-
+//sun settings 
 let effectController = {
     turbidity: 15,
     rayleigh: 2,
@@ -97,24 +88,11 @@ renderer.toneMappingExposure = 0.2;
 
 
 
-
-
-let light = new THREE.HemisphereLight(0xaaaaff, 0xddddff, 1);
-let helper = new THREE.HemisphereLightHelper(light, 5);
-//scene.add(helper);
-//scene.add(light);
-
-
 let scrollY = window.scrollY;
-
-function animate() {
-    requestAnimationFrame(animate);
-    render();
-}
-
-
 let noise = openSimplexNoise.makeNoise4D(Date.now());
 let clock = new THREE.Clock();
+
+
 
 function render() {
 
@@ -141,9 +119,10 @@ function render() {
         geo.attributes.position.needsUpdate = true;
     }
     renderer.render(scene, camera);
+    requestAnimationFrame(render);
 }
 
-animate();
+render();
 
 
 

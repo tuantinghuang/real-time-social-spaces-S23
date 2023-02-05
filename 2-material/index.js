f0c3db5d0e5c0d113b4b99c05ef763d2fe80b82b
tuantinghuang <th2703@nyu.edu>
2023-02-05 15:15:01 -0500

    updates

--- 2-material/index.js
+++ 2-material/index.js
@@ -2,10 +2,8 @@ import * as THREE from 'three'; // * as in everything
 import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 import { RGBELoader } from 'https://threejs.org/examples/jsm/loaders/RGBELoader.js';
 
-import { GLTFLoader } from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
-
 import openSimplexNoise from 'https://cdn.skypack.dev/open-simplex-noise';
-
+import { Sky } from 'https://threejs.org/examples/jsm/objects/Sky.js';
 
 let canvas = document.querySelector('canvas.webgl')
 let renderer = new THREE.WebGLRenderer({
@@ -16,9 +14,14 @@ renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
 
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+//let camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 let cam_r = 3;
-camera.position.z = cam_r;
+//let cam_r = 3;
+//camera.position.z = cam_r;
+//camera.lookAt(0, 0, 0);
+
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000000);
+camera.position.set(0, 0, cam_r);
 camera.lookAt(0, 0, 0);
 
 //make ball
@@ -47,26 +50,58 @@ let mat = new THREE.MeshPhysicalMaterial({
 
 })
 let ball = new THREE.Mesh(geo, mat);
-
 scene.add(ball);
 
-//load gltf (morph)
-// let mesh;
 
-// let loader = new GLTFLoader();
-// loader.load('assets/glTF/AnimatedMorphSphere.gltf', function (gltf) {
+let plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshPhysicalMaterial({
+    color: 0xaaaaaa,
+    side: THREE.DoubleSide
 
-//     mesh = gltf.scene.getObjectByName('AnimatedMorphSphere');
-//     mesh.rotation.z = Math.PI / 2;
-//     //scene.add(mesh);
-//     //console.log(mesh);
-//     mesh.material.dispose();
-//     mesh.material = mat;
+}));
 
-// });
+// Add Sky
+let sky = new Sky();
+sky.scale.setScalar(450000);
+scene.add(sky);
+
+let sun = new THREE.Vector3();
+
+/// GUI
+
+let effectController = {
+    turbidity: 15,
+    rayleigh: 2,
+    mieCoefficient: 0.03,
+    mieDirectionalG: 0.98,
+    elevation: 90,
+    azimuth: 180,
+    exposure: renderer.toneMappingExposure
+};
+
+let uniforms = sky.material.uniforms;
+uniforms['turbidity'].value = effectController.turbidity;
+uniforms['rayleigh'].value = effectController.rayleigh;
+uniforms['mieCoefficient'].value = effectController.mieCoefficient;
+uniforms['mieDirectionalG'].value = effectController.mieDirectionalG;
+
+const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+
+sun.setFromSphericalCoords(1, phi, theta);
+
+uniforms['sunPosition'].value.copy(sun);
+
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 0.2;
 
 
-let light = new THREE.HemisphereLight(0xaaaaff, 0xddddff, 100);
+
+
+
+let light = new THREE.HemisphereLight(0xaaaaff, 0xddddff, 1);
+let helper = new THREE.HemisphereLightHelper(light, 5);
+//scene.add(helper);
 //scene.add(light);
 
 
@@ -86,9 +121,11 @@ function render() {
     if (scrollY) {
         ball.rotation.x = scrollY / 100;
 
-        let r = THREE.MathUtils.degToRad(scrollY);
+        let r = THREE.MathUtils.degToRad(scrollY / 10);
         camera.position.z = cam_r * Math.sin(r);
         camera.position.y = cam_r * Math.cos(r);
+        camera.position.z = cam_r * Math.sin(r);
+
         camera.lookAt(0, 0, 0);
     };
 
